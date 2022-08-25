64163cd681718ac9b42403e657ec1693702ac014
tomyteo123 <huycmdt@gmail.com>
2022-08-26 01:53:37 +0700

    lan1

---
+++ src/Package/Cat.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class Cat {
+	private int tuoi;
+	public void setTuoi(int tuoi) {
+		this.tuoi=tuoi;
+	}
+	public int getTuoi() {
+		return tuoi;
+	}
+}
---
+++ src/Package/Main.java
@@ -0,0 +1,9 @@
+package Package;
+
+public class Main {
+	public static void main(String[] args) {
+		Cat hai = new Cat();
+		hai.setTuoi(18);
+		System.out.println(hai.getTuoi());
+	}
+}
